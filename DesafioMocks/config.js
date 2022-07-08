8886ed80cfbeeaab4a1d2e1e895076c1fea61c80
lbpecoraro <lbpecoraro@gmail.com>
2022-07-08 14:02:33 -0300

    Mocks y normalizr

---
+++ DesafioMocks/config.js
@@ -0,0 +1,7 @@
+export default {
+    mongodb: {
+        mongo: "mongodb+srv://lbpecoraro:lucia3600@cluster0.pi3nb2l.mongodb.net/?retryWrites=true&w=majority",
+        db: "ecommerce",
+        collectionMessage: "mensajes"
+    }
+}
